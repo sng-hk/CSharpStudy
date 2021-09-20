fc76029ebd5b66e7001f7977955606ea06d19a36
최승혁 <csh091456743@gmail.com>
2021-09-20 05:59:45 +0000

    2121-09-20 기초강의 정수

---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace CSharpStudy
+	
+{
+	/* */
+	class Program{
+		static void Main(string[] args) 
+		{
+			int hp;
+			hp = 100;
+			Console.WriteLine("Hello World {0}", hp);
+		}
+	}
+}
