ff9662e1906a5bea3f491676b28ae1d16f206b7e
최승혁 <csh091456743@gmail.com>
2021-09-30 11:08:04 +0000

    dd

--- src/Program.cs
+++ src/Program.cs
@@ -3,58 +3,25 @@ using System;
 namespace CSharpStudy
 	
 {
-	/* 
-	주석
-	*/
+	
 	class Program{
+		enum Choice
+		{
+			a,
+			b,
+			c
+		}
 		static void Main(string[] args) 
 		{
-			Random rand = new Random();
-			int aiChoice = rand.Next(0,3); // 0~2 사이의 랜덤 값		
-			int choice = Convert.ToInt32(Console.ReadLine());
-			
-			bool win;
-			
+			int choice = 0;
 			switch (choice)
 			{
-				case 0:
-					Console.WriteLine("당신: 가위");
+				case (int)Choice.a:
+					Console.WriteLine("choice equals a");
 					break;
-				case 1:
-				Console.WriteLine("당신: 바위");
-				break;
-				case 2:
-				Console.WriteLine("당신: 보");
-				break;
-			}
-			
-			switch (aiChoice)
-			{
-				case 0:
-					Console.WriteLine("나: 가위");
+				default:
+					Console.WriteLine("xx");
 					break;
-				case 1:
-					Console.WriteLine("나: 바위");
-					break;
-				case 2:
-					Console.WriteLine("나: 보");
-					break;
-			}
-			
-			if (choice == aiChoice)
-			{
-				Console.WriteLine("무승부입니다.");
-			}
-			else 
-			{
-				if (choice == 0 && aiChoice == 2 || choice == 1 && aiChoice == 0 || choice == 2 && aiChoice == 1)
-				{
-					Console.WriteLine("승리입니다.");
-				}
-				else
-				{
-					Console.WriteLine("패배입니다.");
-				}
 			}
 		}
 	}
